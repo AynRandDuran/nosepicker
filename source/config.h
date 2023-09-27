9d04b3d26a263f2674d4e3edb1235e36364bd3e3
randy <randy@mcshandy.xyz>
2023-09-26 20:18:46 -0500

    Quickfix config formatting after name changes

--- source/config.h
+++ source/config.h
@@ -15,19 +15,19 @@ Window_Layout config_layout = {
 	},
 
 	// satisfying rel.[xy]*2 == .[wh] centers axis in parent container
-	.hsl_square.rel			=	(SDL_FRect){.x = 0.05, .y = 0.05, .w = 0.50, .h = 0.50},
-	.hue_slider.rel			=	(SDL_FRect){.x = 0.70, .y = 0.05, .w = 0.08, .h = 0.50},
-	.final_sample.rel		=	(SDL_FRect){.x = 0.05, .y = 0.65, .w = 0.20, .h = 0.20},
-	.info_container.rel	=	(SDL_FRect){.x = 0.05, .y = 0.65, .w = 0.90, .h = 0.30},
-	.info_boxes.rel			=	(SDL_FRect){.x = 0.25, .y = 0.00, .w = 0.75, .h = 1.00},
-	.rgb_info.rel				=	(SDL_FRect){.x = 0.00, .y = 0.00, .w = 1.00, .h = 0.50},
-	.red_component.body.rel						=	(SDL_FRect){.x = 0.00, .y = 0.00, .w = 0.30, .h = 1.00},
-	.green_component.body.rel					=	(SDL_FRect){.x = 0.35, .y = 0.00, .w = 0.30, .h = 1.00},
-	.blue_component.body.rel						=	(SDL_FRect){.x = 0.70, .y = 0.00, .w = 0.30, .h = 1.00},
-	.hsl_info.rel				=	(SDL_FRect){.x = 0.00, .y = 0.50, .w = 1.00, .h = 0.50},
-	.hue_component.body.rel						=	(SDL_FRect){.x = 0.00, .y = 0.00, .w = 0.30, .h = 1.00},
-	.sat_component.body.rel			=	(SDL_FRect){.x = 0.35, .y = 0.00, .w = 0.30, .h = 1.00},
-	.lum_component.body.rel			=	(SDL_FRect){.x = 0.70, .y = 0.00, .w = 0.30, .h = 1.00}
+	.hsl_square.rel						=	(SDL_FRect){.x = 0.05, .y = 0.05, .w = 0.50, .h = 0.50},
+	.hue_slider.rel						=	(SDL_FRect){.x = 0.70, .y = 0.05, .w = 0.08, .h = 0.50},
+	.final_sample.rel					=	(SDL_FRect){.x = 0.05, .y = 0.65, .w = 0.20, .h = 0.20},
+	.info_container.rel				=	(SDL_FRect){.x = 0.05, .y = 0.65, .w = 0.90, .h = 0.30},
+	.info_boxes.rel						=	(SDL_FRect){.x = 0.25, .y = 0.00, .w = 0.75, .h = 1.00},
+	.rgb_info.rel							=	(SDL_FRect){.x = 0.00, .y = 0.00, .w = 1.00, .h = 0.50},
+	.red_component.body.rel		=	(SDL_FRect){.x = 0.00, .y = 0.00, .w = 0.30, .h = 1.00},
+	.green_component.body.rel	=	(SDL_FRect){.x = 0.35, .y = 0.00, .w = 0.30, .h = 1.00},
+	.blue_component.body.rel	=	(SDL_FRect){.x = 0.70, .y = 0.00, .w = 0.30, .h = 1.00},
+	.hsl_info.rel							=	(SDL_FRect){.x = 0.00, .y = 0.50, .w = 1.00, .h = 0.50},
+	.hue_component.body.rel		=	(SDL_FRect){.x = 0.00, .y = 0.00, .w = 0.30, .h = 1.00},
+	.sat_component.body.rel		=	(SDL_FRect){.x = 0.35, .y = 0.00, .w = 0.30, .h = 1.00},
+	.lum_component.body.rel		=	(SDL_FRect){.x = 0.70, .y = 0.00, .w = 0.30, .h = 1.00}
 };
 
 // For now we need a full path, maybe OpenFont can do automatic lookup
